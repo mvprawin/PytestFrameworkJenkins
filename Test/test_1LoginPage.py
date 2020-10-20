87d804e35dca78cc6dea27fac8bc9d9543705252
mvprawin <prawin.mattool07@gmail.com>
2020-10-20 18:35:16 +0530

    Allure report included

--- Test/test_1LoginPage.py
+++ Test/test_1LoginPage.py
@@ -1,20 +1,27 @@
 from Config.config import TestData
 from Test.test_base import BaseTest
 from PageObjects.LoginPage import LoginPage
+import allure
 
 
+@allure.epic("OrangeHRM web Application")
+@allure.feature("OrangeHRM Login Page test")
 class Test_LoginPage(BaseTest):
 
+    @allure.story("Login Page test with valid data testcases")
+    @allure.severity(allure.severity_level.NORMAL)
     def test_loginpage_testcase(self):
         global loginpage
         self.loginpage = LoginPage(self.driver)
         self.login_page_title()
         self.login_with_credentials()
 
+    @allure.step("Login Page title check")
     def login_page_title(self):
         title = self.loginpage.get_login_page_title(TestData.LOGIN_PAGE_TITLE)
         assert title == TestData.LOGIN_PAGE_TITLE
 
+    @allure.step("Login with valid data")
     def login_with_credentials(self):
         self.loginpage.do_login(TestData.USERNAME, TestData.PASSWORD)
 
--- Test/test_2HomePage.py
+++ Test/test_2HomePage.py
@@ -1,11 +1,17 @@
+import allure
+
 from Config.config import TestData
 from PageObjects.HomePage import HomePage
 from PageObjects.LoginPage import LoginPage
 from Test.test_base import BaseTest
 
 
+@allure.epic("OrangeHRM web Application")
+@allure.feature("OrangeHRM Home Page test")
 class Test_HomePage(BaseTest):
 
+    @allure.story("Home Page test testcases")
+    @allure.severity(allure.severity_level.NORMAL)
     def test_homepage_testcase(self):
         global loginpage
         self.loginpage = LoginPage(self.driver)
@@ -17,17 +23,21 @@ class Test_HomePage(BaseTest):
         self.account_name_check()
         self.marketplace_link_check()
 
+    @allure.step("Login with valid data")
     def login_check(self):
         self.loginpage.do_login(TestData.USERNAME, TestData.PASSWORD)
 
+    @allure.step("Home Page title check")
     def home_page_title_check(self):
         title = self.homepage.get_home_page_title(TestData.HOME_PAGE_TITLE)
         assert title == TestData.HOME_PAGE_TITLE
 
+    @allure.step("Account name displayed check")
     def account_name_check(self):
         account = self.homepage.get_account_name()
         assert account == TestData.ACCOUNT_NAME
 
+    @allure.step("Marketplace link redirection check")
     def marketplace_link_check(self):
         self.homepage = HomePage(self.driver)
         self.homepage.click_marketplace()
